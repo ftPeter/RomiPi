c36050313de4d53aa81efb1a78874869a8ee8a3c
Jaemarie Solyst <solys22j@mtholyoke.edu>
2018-09-26 16:12:31 -0400

    utility for manually controlling lights

---
+++ pi/pixel_utility.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# pixels_utility.py
+#
+# Manually control the Neopixel colors
+# on the Romi
+# 
+# Peter F. Klemperer
+# September 26, 2018
+from a_star import AStar
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('red', type=int, 
+                    help='red (0-255) value of pixel')
+parser.add_argument('green', type=int, 
+                    help='green (0-255) value of pixel')
+parser.add_argument('blue', type=int, 
+                    help='blue (0-255) value of pixel')
+args = parser.parse_args()
+print(args)
+
+romi = AStar()
+
+# turn on pixel according to arguments
+romi.pixels(args.red, args.green, args.blue)
+
