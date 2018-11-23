94f7605981b4101ef4fed97b57a18be45289c887
Jaemarie Solyst <solys22j@mtholyoke.edu>
2018-11-23 01:59:37 -0500

    cleanup, not sure what those files were for.

--- pi/server.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 1234))
-server_socket.listen(5)
-while True:
-    print("accepting clients...")
-    (client_socket, address) = server_socket.accept()
-
