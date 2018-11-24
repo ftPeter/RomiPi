1395ec4c355bc0f5846ecfb6cf79fca183520801
Peter Klemperer <peter@peterklemperer.com>
2018-11-23 23:21:46 -0600

    fixed motor directions and seems to basically work, but no integral tuning

--- sketch_arduino/Romi-RPi-I2CSlave/hardware.ino
+++ sketch_arduino/Romi-RPi-I2CSlave/hardware.ino
@@ -50,7 +50,7 @@ const int16_t ENCODER_WIN_HIGH = MAX_INT16 * 0.7;
 const int16_t ENCODER_WIN_LOW  = MIN_INT16 * 0.7;
 
 int flip_encoder_dir = -1;
-bool swap_encoders = false;
+bool swap_encoders = true;
 
 int hw_getencoder_left() {
   if ( swap_encoders ) {
