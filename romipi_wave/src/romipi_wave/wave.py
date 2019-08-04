c15aaa978ac02fa315b466fd9dc7528f32737f59
Jaemarie Solyst <solys22j@mtholyoke.edu>
2019-08-04 19:48:27 -0400

    initial add of the wave

---
+++ romipi_wave/src/romipi_wave/wave.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+class WaveNode():
+    def __init__(self, name, assigned_wave, node_list):
+        """ open the wave node """
+        self.name = name
+        self.assigned_wave = assigned_wave
+        self.node_list = node_list
+        print(self)
+        return
+
+    def __str__(self):
+        return "WaveNode (%s) assigned wave (%d) with node_list %s" % ( \
+                self.name, self.assigned_wave, str(self.node_list))
+
+    def test_node():
+        return
+
+if __name__ == '__main__':
+    try:
+        wave = WaveNode("jiffy.local", 1, node_list)
+        wave.test_node()
+    except rospy.ROSInterruptException:
+        pass
+
